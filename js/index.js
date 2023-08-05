cd36a0c3337a2d6f6bd7e644c885a7ab936ce48c
2ytka2 <dassina222@gmail.com>
2023-08-05 15:21:17 +0300

    fix separator and fix modal menu

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
 const openMenuButton = document.querySelector('.nav__hide')
 const closeMenuButton = document.querySelector('.menu__close');
-
-openMenuButton.addEventListener('click', function(){
-    const menuOpen = document.querySelector('.menu__open');
+const menuOpen = document.querySelector('.menu__open');
+openMenuButton.addEventListener('click', function(e){
+    e.preventDefault();
     menuOpen.style.display = 'flex';
 })
 
-closeMenuButton.addEventListener('click', function(){
-    const menuOpen = document.querySelector('.menu__open');
-    menuOpen.style.display = 'none';
+closeMenuButton.addEventListener('click', function(e){
+   e.preventDefault();
+});
+
+menuOpen.addEventListener('click', function(e){
+    const targets = e.target.classList
+    if(!targets.contains('menu__open-list') && !targets.contains('menu__open-item') && !targets.contains('menu__open-link')){
+        console.log(e.target);
+        menuOpen.style.display = 'none';
+    }
 })
+
+
+
