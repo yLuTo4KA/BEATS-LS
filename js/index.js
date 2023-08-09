c61cc18db40991888526cfe2c9f790b4e915a835
2ytka2 <dassina222@gmail.com>
2023-08-09 13:44:28 +0300

    add slider

--- js/index.js
+++ js/index.js
@@ -33,5 +33,18 @@ $('.team__name').on('click', function(e){
 });
 
 //// slider 
+$('.slider__hide-info-btn').on('click', function(e){
+    e.preventDefault();
+});
+$('.arrow').on('click', function(e){
+    e.preventDefault();
+});
 
+    
 
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow: $('.slider__btn--left'),
+        nextArrow: $('.slider__btn--right')
+    });
+  });
