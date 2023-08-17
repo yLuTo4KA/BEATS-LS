a71337438e459cd002f4760bf244f133876bdb39
2ytka2 <dassina222@gmail.com>
2023-08-17 20:47:28 +0300

    add YMap

--- js/index.js
+++ js/index.js
@@ -359,3 +359,41 @@ function onYouTubeIframeAPIReady() {
 };
 eventsInit();
 
+
+
+/// map
+
+
+let myMap;
+
+function init(){
+    myMap = new ymaps.Map('map', {
+        center: [51.124295, 71.456133],
+        zoom: 12,
+        controls: ['typeSelector', 'zoomControl']
+    });
+    const coords = [
+        [51.124295, 71.456133],
+        [51.089407, 71.418265],
+        [51.148425, 71.420914],
+        [51.178351, 71.419548]
+    ];
+    let myCollection = new ymaps.GeoObjectCollection({},{
+        draggable: false,
+        iconLayout: 'default#image',
+        iconImageHref: './img/icons/decoration/marker.svg',
+        icon_imagesize: [58, 73],
+        iconImageOffset: [-3, -42]
+    })
+    coords.forEach(coord => {
+        myCollection.add(new ymaps.Placemark(coord));
+    })
+    myMap.geoObjects.add(myCollection);
+    myMap.behaviors.disable('scrollZoom');
+    
+   
+}
+
+
+
+ymaps.ready(init);
